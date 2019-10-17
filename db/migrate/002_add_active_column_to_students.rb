c2537ead7ed59ce08efffe363d0c484c66a87b16
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-17 00:54:36 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_column_to_students.rb
+++ db/migrate/002_add_active_column_to_students.rb
@@ -1,5 +1,5 @@
-class AddActiveComlumnToStudents < ActiveRecord::Migration
-  def change
-    add_column :students, :active, :boolean, default: false 
-  end
-end 
+# class AddActiveComlumnToStudents < ActiveRecord::Migration
+#   def change
+#     add_column :students, :active, :boolean, default: false 
+#   end
+# end 
